2fab3320f067d22eb930f13744cc7a58607f55e5
senterv2 <arifindorleng2498@gmail.com>
2026-04-22 17:38:04 +0700

    1

---
+++ Backend/Data/Entities/tbAuditLogs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Data.Entities;
+
+public partial class tbAuditLogs
+{
+    public long nId { get; set; }
+
+    public int? nUserId { get; set; }
+
+    public string sAction { get; set; } = null!;
+
+    public string? sDescription { get; set; }
+
+    public string? sIpAddress { get; set; }
+
+    public DateTime? dActionDate { get; set; }
+
+    public virtual tmUsers? nUser { get; set; }
+}

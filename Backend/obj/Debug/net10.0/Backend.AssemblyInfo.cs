82454b98cd6f99d4c42ac7686176f4d442d9b5cd
senterv2 <arifindorleng2498@gmail.com>
2026-04-24 09:19:05 +0700

    Remove bin and obj folders from git tracking

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9029b11eac87b2a4888ad8d7034895b84190d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce85d3f03c3682f661e16392aaad348d289ea2df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

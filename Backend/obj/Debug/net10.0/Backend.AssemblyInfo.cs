fe470212d25d189a1399980eeab3212e232973b5
senterv2 <arifindorleng2498@gmail.com>
2026-04-27 09:32:01 +0700

    chore: save current state before merging ping

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d12d355b99523ceab6ecd6f8d6ac45839283fff3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995306dfd8658ce312c33ff3eaca25e633f20efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
